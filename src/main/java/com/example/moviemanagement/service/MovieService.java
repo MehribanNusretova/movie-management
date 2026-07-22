f62a93414652bc147df82119d754261a049594c2
Mehri <mehribannusretova2020@gmail.com>
2026-07-22 17:28:08 +0400

    Add service interfaces

--- src/main/java/com/example/moviemanagement/service/MovieService.java
+++ src/main/java/com/example/moviemanagement/service/MovieService.java
@@ -1,4 +1,16 @@
 package com.example.moviemanagement.service;
 
+import com.example.moviemanagement.dto.MovieRequestDto;
+import com.example.moviemanagement.dto.MovieResponseDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface MovieService {
+    MovieResponseDto createMovie(MovieRequestDto movieRequestDto);
+    List<MovieResponseDto> getAllMovies();
+    MovieResponseDto getMovieById(Long id);
+    MovieResponseDto updateMovie(Long id, MovieRequestDto movieRequestDto);
+    void deleteMovie(Long id);
 }
