f3800d169f15793d868ab59f1500919e3b81d83a
Mehri <mehribannusretova2020@gmail.com>
2026-07-22 18:12:58 +0400

    Add DTO classes

--- src/main/java/com/example/moviemanagement/dto/MovieRequestDto.java
+++ src/main/java/com/example/moviemanagement/dto/MovieRequestDto.java
@@ -1,4 +1,15 @@
 package com.example.moviemanagement.dto;
 
 public class MovieRequestDto {
+    private String title;
+
+    private String description;
+
+    private Integer releaseYear;
+
+    private Double rating;
+
+    private Long directorId;
+
+    private Long genreId;
 }
--- src/main/java/com/example/moviemanagement/dto/MovieResponseDto.java
+++ src/main/java/com/example/moviemanagement/dto/MovieResponseDto.java
@@ -1,4 +1,17 @@
 package com.example.moviemanagement.dto;
 
 public class MovieResponseDto {
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private Integer releaseYear;
+
+    private Double rating;
+
+    private String directorName;
+
+    private String genreName;
 }
