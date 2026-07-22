a04adfd1da88f507729a2559119e78f11e13ca5c
Mehri <mehribannusretova2020@gmail.com>
2026-07-22 22:59:26 +0400

    add @RestControllerAdvice annotation

--- src/main/java/com/example/moviemanagement/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/moviemanagement/exception/GlobalExceptionHandler.java
@@ -6,11 +6,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
 
