766d81ca0199ae65ec4dbf4c6288cdff8b645ef9
Mehri <mehribannusretova2020@gmail.com>
2026-07-22 19:00:16 +0400

    add bean validation annotations to MovieRequestDto

--- src/main/java/com/example/moviemanagement/dto/MovieRequestDto.java
+++ src/main/java/com/example/moviemanagement/dto/MovieRequestDto.java
@@ -1,5 +1,9 @@
 package com.example.moviemanagement.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,15 +14,24 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MovieRequestDto {
+
+    @NotBlank(message = "Title cannot be empty")
     private String title;
 
+    @NotBlank(message = "Description cannot be empty")
     private String description;
 
+    @NotNull(message = "Release year is required")
     private Integer releaseYear;
 
+    @NotNull(message = "Rating is required")
+    @Min(value = 0, message = "Rating cannot be less than 0")
+    @Max(value = 10, message = "Rating cannot be greater than 10")
     private Double rating;
 
+    @NotNull(message = "Director is required")
     private Long directorId;
 
+    @NotNull(message = "Genre is required")
     private Long genreId;
 }
