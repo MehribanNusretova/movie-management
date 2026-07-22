9cb5571b3722eb7b94665508de2cd012547a2ea8
Mehri <mehribannusretova2020@gmail.com>
2026-07-22 18:23:33 +0400

    add MovieMapper for DTO and entity conversion

--- src/main/java/com/example/moviemanagement/mapper/MovieMapper.java
+++ src/main/java/com/example/moviemanagement/mapper/MovieMapper.java
@@ -1,4 +1,38 @@
 package com.example.moviemanagement.mapper;
 
+import com.example.moviemanagement.dto.MovieRequestDto;
+import com.example.moviemanagement.dto.MovieResponseDto;
+import com.example.moviemanagement.entity.Movie;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MovieMapper {
+    public Movie toEntity(MovieRequestDto requestDto) {
+
+        Movie movie = new Movie();
+
+        movie.setTitle(requestDto.getTitle());
+        movie.setDescription(requestDto.getDescription());
+        movie.setReleaseYear(requestDto.getReleaseYear());
+        movie.setRating(requestDto.getRating());
+
+        return movie;
+    }
+    public MovieResponseDto toResponseDto(Movie movie) {
+
+        MovieResponseDto response = new MovieResponseDto();
+
+        response.setId(movie.getId());
+        response.setTitle(movie.getTitle());
+        response.setDescription(movie.getDescription());
+        response.setReleaseYear(movie.getReleaseYear());
+        response.setRating(movie.getRating());
+
+        response.setDirectorName(movie.getDirector().getName());
+
+        response.setGenreName(movie.getGenre().getName());
+
+        return response;
+    }
+
 }
